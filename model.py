eb9e5ad14638b6d3417e064749509525ad0ada13
darshan <darshan.paccha@gmail.com>
2025-08-04 01:08:18 +0530

    fix: update pinecone logic

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain_community.vectorstores import Pinecone as PineconeVectorStore
+from langchain_pinecone import Pinecone as PineconeVectorStore # ← Updated import
 
 load_dotenv()
 
@@ -11,6 +11,8 @@ embedding   = OpenAIEmbeddings()
 index_name  = os.getenv("PINECONE_INDEX_NAME") or "aurag"
 
 def query_document(query: str):
+    # This LangChain function now correctly initializes the new Pinecone client
+    # using environment variables under the hood.
     vectorstore = PineconeVectorStore.from_existing_index(
         index_name=index_name,
         embedding=embedding,
@@ -18,4 +20,5 @@ def query_document(query: str):
     )
     docs  = vectorstore.similarity_search(query, k=5)
     chain = load_qa_chain(llm, chain_type="stuff")
-    return chain.run(input_documents=docs, question=query)
+    # chain.run is deprecated, prefer .invoke for new LangChain versions
+    return chain.invoke({"input_documents": docs, "question": query})['output_text']
